430a8e3f2f9df1b2d7d97db3c58c310ab5707dc6
Oribi <oribimegoodluck@outlook.com>
2024-09-10 12:36:24 +0100

    added file

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+// const plugin = require('tailwindcss/plugin')
+
+// module.exports = {
+//   plugins: [
+//     plugin(function({ addUtilities, addComponents, e, prefix, config }) {
+//         const newUtilities = {
+//           '.horizontal-tb': {
+//             writingMode: 'horizontal-tb',
+//           },
+//           '.vertical-rl': {
+//             writingMode: 'vertical-rl'
+//           },
+//           '.vertical-lr': {
+//             writingMode: 'vertical-lr'
+//           }
+//         }
+//         addUtilities(newUtilities)
+//       }),
+//   ]
+// }
